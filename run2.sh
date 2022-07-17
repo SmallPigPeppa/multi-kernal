4ea497a894857222eed0b98f95dfed3c529c6148
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-07-17 23:59:07 +0800

    test fix encoder

---
+++ run2.sh
@@ -0,0 +1,18 @@
+python3 pretrain.py \
+    --dataset cifar100 \
+    --data_dir ~/torch_ds/ \
+    --image_size 224 \
+    --max_epochs 200 \
+    --precision 16 \
+    --optimizer sgd \
+    --exclude_bias_n_norm \
+    --scheduler warmup_cosine \
+    --lr 0.4 \
+    --classifier_lr 0.1 \
+    --weight_decay 1e-5 \
+    --batch_size 256 \
+    --num_workers 5 \
+    --name cifar100 \
+    --project multi-resolution \
+    --entity pigpeppa \
+    --wandb
